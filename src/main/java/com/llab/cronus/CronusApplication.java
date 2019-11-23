b4f3e74dd5b2a7e643381b793cb53596400b2522
root <root@172.30.63.211>
2019-11-24 01:03:55 +0900

    - demo to init project

---
+++ src/main/java/com/llab/cronus/CronusApplication.java
@@ -0,0 +1,17 @@
+package com.llab.cronus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(value = { "com.llab.cronus.config", "com.llab.cronus.controller", "com.llab.cronus.service" })
+@EntityScan(value = { "com.llab.cronus.entity" })
+public class CronusApplication {
+	
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(CronusApplication.class, args);
+	}
+}
---
+++ src/main/java/com/llab/cronus/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.llab.cronus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = { "/dashboard" })
+public class DashboardController {
+	@RequestMapping(value = { "/system" })
+	public String system() {
+		return "/dashboard/system";
+	}
+}
