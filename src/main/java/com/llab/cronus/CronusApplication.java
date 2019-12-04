81ca774524d9a821c81250d77c83ea9b883aeeaa
root <root@172.30.63.211>
2019-12-04 23:29:15 +0900

    - elastic data test

--- src/main/java/com/llab/cronus/CronusApplication.java
+++ src/main/java/com/llab/cronus/CronusApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = { "com.llab.cronus.config", "com.llab.cronus.controller", "com.llab.cronus.service" })
-@EntityScan(value = { "com.llab.cronus.entity" })
+@EntityScan(value = { "com.llab.cronus.entity", "com.llab.cronus.repository" })
 public class CronusApplication {
 	
 	public static void main(String[] args) {
--- src/main/java/com/llab/cronus/controller/DashboardController.java
+++ src/main/java/com/llab/cronus/controller/DashboardController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = { "/dashboard" })
 public class DashboardController {
+
 	@RequestMapping(value = { "/system" })
 	public String system() {
 		return "/dashboard/system";
 	}
+
 }
